505a428f937ebad6bac5ba5baab3332c02d171f2
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-08-07 11:04:43 +0100

    test: write dummy test for hello world component

---
+++ packages/nen-ui/test/HelloWorld.test.ts
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import HelloWorld from '../src/components/HelloWorld.vue'
+
+describe('HelloWorld.vue', () => {
+  it('should render', () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'I am a message' } })
+    expect(wrapper.text()).toContain('I am a message')
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('should be interactive', async () => {
+    const wrapper = mount(HelloWorld, { props: { msg: 'I am a message' } })
+    expect(wrapper.text()).toContain('I am a message')
+
+    expect(wrapper.find('.increment').exists()).toBe(true)
+
+    expect(wrapper.find('.dec').exists()).toBe(false)
+
+    await wrapper.get('.increment').trigger('click')
+
+    expect(wrapper.text()).toContain('1')
+  })
+})
