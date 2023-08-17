df7099e90b02a117de28a6e0ba00246707dfdcf9
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-08-17 21:59:19 +0100

    chore: remove build command in publish script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -5,8 +5,6 @@ import { version } from '../package.json'
 
 let name = 'nen-ui'
 
-execSync('pnpm run build', { stdio: 'inherit', cwd: path.join('packages', name) })
-
 let command = 'npm publish --access public'
 
 if (version.includes('beta'))
